3e2e327b9b8d90029aae55ca42903aca92594b8c
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-13 17:30:13 +0530

    inital and add sass

---
+++ ticktoe/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  Board  from './components/Board'
+import "./styles/root.scss";
+
+ const App =()=>{
+   return (
+<div className="app">
+    <h1>Tic Toe</h1>
+    
+  
+    <Board/>
+  </div>
+   );
+ };
+export default  App;
---
+++ ticktoe/src/components/Square.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Square =({value}) => {
+ 
+    return (
+    <button  type="button" className="square" >
+        {value}
+        </button>
+        )
+
+}
+
+export default Square
